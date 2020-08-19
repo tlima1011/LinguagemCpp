6ed46e0369b8c2f970bfa0207c7f548aeba195a0
Thiago <thiagolima1011@gmail.com>
2020-08-19 09:51:38 -0300

    Menor de Tres

---
+++ menor_de_tres/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, menor;
+
+    cout << "Primeiro valor: ";
+    cin >> a;
+    cout << "Segundo valor: ";
+    cin >> b;
+    cout << "Terceiro valor: ";
+    cin >> c;
+    if(a < b && a < c){
+        menor = a;
+    }else if(b < a && b < c){
+        menor = b;
+    }else{
+        menor = c;
+    }
+    cout << "MENOR = " << menor;
+
+    return 0;
+}
